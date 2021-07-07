b9e2da5d8eb57eea9790e63ee310a85ac6f4bd68
henryglasse1151 <hdglasse2@gmail.com>
2021-07-07 19:29:25 +0100

    hello

--- source/icon.py
+++ source/icon.py
@@ -19,8 +19,8 @@ class Icon:
         width = kwargs.get('width', -1)
         height = kwargs.get('height', -1)
         exd = Globals.execution_directory
-        icon_folder = exd + '/Icons'
-        iconfile = icon_folder + '/' + name + '.png'
+        icon_folder = exd + '\\..\\Icons'
+        iconfile = icon_folder + '\\' + name + '.png'
         img = Image.open(iconfile)
         if width > 0:
             wpercent = (width / float(img.size[0]))
